41af3b1a146fc51739c7ae010ebe20157b0e6084
Yannick Luthiger <yannick.luthiger@stud.hslu.ch>
2018-11-24 10:34:05 +0100

    changed local server address from 0.0.0.0 to localhost

--- app/__main__.py
+++ app/__main__.py
@@ -22,4 +22,4 @@ def move():
 
 application = bottle.default_app()
 if __name__ == '__main__':
-    bottle.run(application, host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', '8080'))
+    bottle.run(application, host=os.getenv('IP', 'localhost'), port=os.getenv('PORT', '8080'))
